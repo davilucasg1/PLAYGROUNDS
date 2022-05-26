6d8adc2c1472e44c037a301ae677ae67f6c539bf
Davi Lucas Gomes Feitosa <dlfeitosa@stefanini.com>
2022-05-25 22:31:28 -0300

    structs

---
+++ struct.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+struct Person {
+    var name: String
+    var age: Int
+    
+    func sayHello() {
+        print("hello")
+    }
+    
+    mutating func incremnetAge() {
+        age += 1
+    }
+}
+
+var person = Person(name: "davizin", age: 23)
+person.sayHello()
+person.name = "pedrinho"
+print(person.name)
+
+// propriedades computadas
+
+struct Temperature{
+    var celsius: Double
+    var fahrenheit: Double {
+        return celsius * 1.8 + 32
+    }
+}
+
+ var temperature = Temperature(celsius: 21)
+print(temperature.celsius)
+print(temperature.fahrenheit)
